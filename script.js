f9140f19885c5de135e143f5d66017996e4a2ab1
Anton <example@gmail.com>
2020-05-13 16:59:21 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+"use strict";
+
+//alert('Hello')
+
+/**const result = confirm('Are you?');
+console.log(result);
+
+const answer = prompt('Are you 18','');
+conso
+
+const answer = [];
+answer[0] = promt('Hey you','');
+answer[1] = promt('Hey you surname','');
+answer[2] = promt('Hey youur age','');
+
+console.log(typeof(answer));
+
+const categ = 'toys';
+console.log(`http://someutr.com/${categ}/5`);
+
+const user = 'ant';
+alert(`Hi, ${user}`);
+
+console.log ('arr' + '- object');
+console.log (4 + +'5');
+
+let incr = 10,
+    decr = 10;
+
+//++incr; 
+//--decr;
+
+console.log(incr++);
+console.log(decr--);*/
+
+const isChek = false,
+        isClose = true;
+
+console.log(isChek || isClose);
+
+
+
+
+
+
